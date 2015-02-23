afd23a8a9ce3fe887a7eb74375cbced533fd1822
Miranda Wang <msw3bb@virginia.edu>
2015-02-23 16:37:15 -0500

    Edited.

--- course_reader.py
+++ course_reader.py
@@ -6,13 +6,12 @@ def load_course_database(db_name, csv_filename):
     print("hello")
     with open(csv_filename, 'rU') as csvfile:
             reader = csv.reader(csvfile)
-            for row in reader:
-                print(row)
+
     conn = sqlite3.connect(db_name)
     with conn:
         cur = conn.cursor()
         sql_cmd = "insert into coursedata values(?, ?, 1122, 'Lecture', 20, 20, 'T. Jefferson')"
-        cur.execute(sql_cmd, (dept,courseNum)) #value of dept used for the first ?, and courseNum for the 2nd
+        cur.execute(sql_cmd, tuple(reader)) #value of dept used for the first ?, and courseNum for the 2nd
 
 
 if __name__ == '__main__':
